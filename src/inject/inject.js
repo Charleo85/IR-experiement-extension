61f1263e0f3de5b96464696f00bc78c501ffeabb
Charlie Wu <wujibang@gmail.com>
2017-04-28 23:38:52 -0400

    icon update

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -25,7 +25,24 @@ chrome.extension.sendMessage({}, function(response) {
 			console.log("Hello. This message was sent from scripts/inject.js");
 			// ----------------------------------------------------------
 
-			//post and receive
+			//post and receive data
+			var http = new XMLHttpRequest();
+			var url = "get_data.php";
+			var params = "lorem=ipsum&name=binny";
+			http.open("POST", url, true);
+
+			//Send the proper header information along with the request
+			http.setRequestHeader("Content-type",
+				"application/x-www-form-urlencoded");
+
+			http.onreadystatechange = function() { //Call a function when the state changes.
+				if (http.readyState == 4) {
+					if (http.status == 200) {
+						alert(http.responseText);
+					}
+				}
+			}
+			http.send(params);
 			//for loop replace
 
 			var word =
