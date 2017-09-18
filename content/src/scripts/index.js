8737c9d6adcdf5473fa926cb5d757e6858f9e947
Charlie Wu <wujibang@gmail.com>
2017-09-18 00:57:22 -0400

    prettier test

--- content/src/scripts/index.js
+++ content/src/scripts/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { render } from "react-dom";
-import { Provider } from "react-redux";
-import { Store } from "react-chrome-redux";
+import {render} from "react-dom";
+import {Provider} from "react-redux";
+import {Store} from "react-chrome-redux";
 
 import App from "./components/app/App";
 
